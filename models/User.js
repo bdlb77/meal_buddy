d6cfdfbf401b8f1348a70b58f8cc72e591166183
Bryan Leighton <bryan.leigh77@gmail.com>
2019-02-15 20:36:04 +0100

    routes and update of user model to handle login

--- models/User.js
+++ models/User.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
+const md5 = require('md5');
 const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
     email: {
@@ -17,7 +20,15 @@ const userSchema = new Schema({
         required: 'Please supply a name!',
         trim: true,
     },
+    // resetPasswordToken: String,
+    // resetPasswordExpires: Date.
 });
+userSchema.virtual('gravatar').get(function(){
+    const hash = md5(this.email);
+    return `https://gravatar.com/avatar/${hash}?size=150`; 
+})
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
 
 
 module.exports = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
+// EXTERNAL
 const express = require('express');
 const router = express.Router();
+// INTERNAL
 const eventController = require('../controllers/eventController');
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
+// EVENTS
 router.get('/homepage', eventController.homePage);
 
 router.get('/events', catchErrors(eventController.getEvents));
 
+router.get('/event/:slug', catchErrors(eventController.getSingleEvent));
+
+router.get('/event/:id/edit', catchErrors(eventController.editSingleEvent));
+
+router.post('/add/:id', catchErrors(eventController.updateEvent));
+
+// AUTHENTICATION
+
+router.get('/users/login', userController.loginForm);
+router.post('/users/login', authController.login);
+
+
 module.exports = router;
+
